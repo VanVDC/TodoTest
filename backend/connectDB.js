6c2ef80667c51dfffcadf2854b90303de45a593d
VanVDC <vanbvy@gmail.com>
2021-11-02 14:21:44 -0400

    Update connectDB.js

--- backend/connectDB.js
+++ backend/connectDB.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const connect = async () => {
   try {
     const conn = await mongoose.connect(
-      'mongodb+srv://pressure:pressure@cluster0.k7xfp.mongodb.net/todo',
+     
       {
         useUnifiedTopology: true,
         useNewUrlParser: true,
